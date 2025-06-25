e54d8c7adf5ed79cc952eed52518d90bed34b97c
Rui <yehruei@163.com>
2025-06-25 15:01:13 +0800

    Update home.vue

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,10 +1,12 @@
 <script setup>
+// Javascript
 import { ref } from 'vue'
 
 const count = ref(0)
 </script>
 
 <template>
+    <!-- HTML -->
     <div class="content">
         <h1>Hello!</h1>
         <p>This is your Home page.</p>
@@ -15,11 +17,29 @@ const count = ref(0)
             <n-image width="120" src="./wendy/hello.gif"></n-image>
         </p>
     </div>
+    <div style="width: 50%">
+        <div class="livingroom">living room</div>
+        <div class="bedroom1">bedroom one</div>
+        <div class="bedroom2">bedroom two</div>
+    </div>
 </template>
 
 <style scoped>
+/* CSS */
 .content {
     text-align: center;
     padding: 2vw;
+}
+
+.livingroom {
+    background-color: pink;
+}
+
+.bedroom1 {
+    background-color: yellow;
+}
+
+.bedroom2 {
+    background-color: green;
 }
 </style>
